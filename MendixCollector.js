0b8f8892d3015737f057fda3e7abb6a9ee69d95b
Kristopher Clark <krisclarkdev@krisbox.org>
2019-05-27 14:52:48 -0500

    first commit

---
+++ MendixCollector.js
@@ -0,0 +1,130 @@
+'use strict';
+
+const args     = require('yargs').argv;
+const axios    = require('axios');
+const metrics  = require('wavefrontmetrics');
+const registry = new metrics.Registry();
+const prefix = "mendix";
+const flatten = require('flat');
+
+let the_interval = args.interval * 1000;
+let interval = args.interval;
+let hostname = args.hostname + ':' + args.port;
+let password = new Buffer(args.password).toString('base64');
+let wfproxy  = args.wfproxy;
+let wfport   = args.wfport;
+
+const proxyReporter = new metrics.WavefrontProxyReporter(registry, prefix, wfproxy, wfport, {'source': "mendix"});
+
+proxyReporter.start(5000);
+
+let headers = {
+    'Content-Type': 'application/json',
+    'X-M2EE-Authentication': password
+};
+
+function addMetric(metric, value, tags) {
+    try {
+        registry.addTaggedMetric(metric, new metrics.Gauge(value), tags);
+    }catch(err){
+        console.log(err);
+    }
+}
+let flattenObject = function(ob) {
+    var toReturn = {};
+
+    for (var i in ob) {
+        if (!ob.hasOwnProperty(i)) continue;
+
+        if ((typeof ob[i]) == 'object') {
+            var flatObject = flattenObject(ob[i]);
+            for (var x in flatObject) {
+                if (!flatObject.hasOwnProperty(x)) continue;
+
+                toReturn[i + '.' + x] = flatObject[x];
+            }
+        } else {
+            toReturn[i] = ob[i];
+        }
+    }
+    return toReturn;
+};
+
+let stageMetrics = function(res) {
+    try {
+        let flatObject = flattenObject(res.data.feedback);
+
+        for(let key in flatObject) {
+            if(!isNaN(flatObject[key])) {
+                addMetric(key, flatObject[key]);
+            }
+        }
+    }catch(ex){
+        console.log('Error flattening response.  Will ignore and continue to the next object.')
+    }
+};
+
+let currentExecutions = function() {
+    axios.post(hostname,{action: "get_current_runtime_requests", params: {}}, {headers: headers})
+        .then((res) => {
+            stageMetrics(res);
+        })
+        .catch((error) => {
+            console.error(error)
+        });
+};
+
+function getRuntimeStats() {
+    axios.post(hostname,{action: "runtime_statistics", params: {}}, {headers: headers})
+        .then((res) => {
+            stageMetrics(res);
+        })
+        .catch((error) => {
+            console.error(error)
+        });
+}
+
+function getStateStats() {
+    axios.post(hostname,{action: "cache_statistics", params: {}}, {headers: headers})
+        .then((res) => {
+            stageMetrics(res);
+        })
+        .catch((error) => {
+            console.error(error)
+        });
+}
+
+function getServerStats(){
+    axios.post(hostname,{action: "server_statistics", params: {}}, {headers: headers})
+        .then((res) => {
+            stageMetrics(res);
+        })
+        .catch((error) => {
+            console.error(error)
+        });
+}
+
+function getLoggedInUsersStats(){
+    axios.post(hostname,{action: "get_logged_in_user_names", params: {}}, {headers: headers})
+        .then((res) => {
+            stageMetrics(res);
+        })
+        .catch((error) => {
+            console.error(error)
+        });
+}
+
+currentExecutions();
+getRuntimeStats();
+getStateStats();
+getServerStats();
+getLoggedInUsersStats();
+
+setInterval(function() {
+    currentExecutions();
+    getRuntimeStats();
+    getStateStats();
+    getServerStats();
+    getLoggedInUsersStats();
+
+}, the_interval);
---
+++ README.md
@@ -0,0 +1,77 @@
+# MendixCollector
+
+This is a NodeJS app that scrapes all available metrics from a MendixApplication and then sends them to a Wavefront proxy for monitoring.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+What things you need to install the software and how to install them
+
+* NodeJS >= 6.4.1
+* NPM >= 6.4.1
+* A Mendix application
+* Your M2EE_ADMIN_PASS
+* Your M2EE_ADMIN_PORT
+* Wavefront Account
+* Wavefront Proxy
+
+### Installing
+
+These instructions are intended for development only.  See production for production instructions.
+
+```
+git clone https://github.com/krisclarkdev/MendixCollector.git
+cd MendixCollector
+npm install
+```
+
+Open package.json and change all CHANGEME values to match your enviornment
+
+## Running the tests
+
+```
+npm run test
+```
+
+This test will make sure you can connect to Mendix and send metrics into Wavefront.  Within a few second you should be 
+able to see metrics in Wavefront coming from the Mendix source.
+
+## Deployment
+
+To deploy you can either download a release for your platform from this repository or you can build from source
+yourself.
+
+### Building from source
+
+```
+npm run build
+```
+
+### Running
+
+To run the binary open a terminal and execute
+
+```
+node ./MendixCollector.js  --interval=30 --hostname=http://CHANGEME --port=CHANGEME --password=CHANGEME --wfproxy=CHANGEME --wfport=CHANGEME
+```
+
+This will produce 
+
+```
+MendixCollector-linux
+MendixCollector-macos
+MendixCollector-win.exe
+```
+
+If you are deploying to linux or mac make sure to chmod +x the binary.
+
+## Authors
+
+* **Kristopher Clark** - *Initial work* - [krisclarkdev](https://github.com/krisclarkdev)
+
+## License
+
+This project is licensed under the Apache 2.0 License - see the [LICENSE.md](LICENSE.md) file for details
